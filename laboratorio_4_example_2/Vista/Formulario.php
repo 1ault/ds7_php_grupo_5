e90c8cf658047400004b91aa254c8e727ff3e132
tuzz <tuzz@localhost.com>
2026-05-15 16:37:11 -0500

    propuesta

--- laboratorio_4_example_2/Vista/Formulario.php
+++
@@ -1,8 +0,0 @@
-<form action="/cookie/set" method="POST">
-    <label for="">Registro</label>
-
-    <label for="nombre">Nombre:</label>
-    <input type="text" name="nombre" placeholder="nombre">
-
-    <button type="submit">Guardar</button>
-</form>
--- laboratorio_5/Modelo/Image.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Root\Program\Modelo;
-
-use Root\Program\Config\Layer8;
