8d149f5904db29de4dab8b6d66b58e4cbca3e39f
tuzz <tuzz@mail.local>
2026-05-17 21:53:54 +0000

    Ajustes

--- parcial_2/Public/index.php
+++ parcial_2/Public/index.php
@@ -1,1 +1,17 @@
 <?php
+<<<<<<< Updated upstream
+=======
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Root\Program\Controlador\UsuarioController;
+
+$controller = new UsuarioController();
+
+$controller->registrar();
+
+$mensaje = $controller->mensaje;
+
+require_once __DIR__ . "/../Vista/Registro.php";
+>>>>>>> Stashed changes
